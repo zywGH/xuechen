afb400b57549c4de928d8a26601823097b7cc7cc
zhyanwei <504395191@qq.com>
2023-07-30 23:12:41 +0800

    [+]day01

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/XuechengPlusContentApiApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/XuechengPlusContentApiApplication.java
@@ -1,9 +1,11 @@
-package com.xuecheng.content.api;
+package com.xuecheng;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class XuechengPlusContentApiApplication {
 
     public static void main(String[] args) {
